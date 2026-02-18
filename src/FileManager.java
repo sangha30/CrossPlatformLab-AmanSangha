e0065bbbe6922334920ee927404414b035861e91
sangh <sangh@AMANPC>
2026-02-18 14:50:47 -0500

    initial commit

---
+++ src/FileManager.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sangh
+ */
+public class FileManager {
+    public static void doSomething(){
+        System.out.println("File Manager running.");
+    }
+}
